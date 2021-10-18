efd2ba133abd6f8488e9f8d2ec861ca247bc43eb
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-10-18 22:59:07 +0900

    [6일차] 29강 - 34강

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -6,15 +6,41 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+  var questions = [
+    '제일 좋아하는 색이 뭐에요?',
+    '제일 좋아하는 동물이 뭐에요?'
+  ];
+
+  void answerQuestion() {
+    questionIndex += 1;
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Text('This is my default text!'),
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('My First App'),
       ),
-    );
+      body: Column(
+        children: [
+          Text(questions[0]),
+          RaisedButton(
+            child: Text('대답 1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('대답 2'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('대답 3'),
+            onPressed: answerQuestion,
+          ),
+        ],
+      ),
+    ));
   }
 }
